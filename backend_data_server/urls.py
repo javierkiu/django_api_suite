affc7a78cfff283843edb179301c92da88efb88e
Javier Rodríguez <81196360+javierkiu@users.noreply.github.com>
2025-07-28 05:10:58 +0000

    guia 20: Django Rest

--- backend_data_server/urls.py
+++ backend_data_server/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("homepage/", include("homepage.urls")),
+    path("demo/rest/api/", include("demo_rest_api.urls"))
 
 ]
